c76dd281c60c956508bd8f3dfac6e5fe083ca492
Rene Hexel <r.hexel@griffith.edu.au>
2018-07-22 17:48:54 +1000

    Update to 2018 version of HAP (following HAP spec 5.5.2)

--- Sources/orvibo-udp-hap/main.swift
+++ Sources/orvibo-udp-hap/main.swift
@@ -18,20 +18,25 @@ enum AccessoryKind: String {
 let args = CommandLine.arguments
 let cmd = args[0]                   ///< command name
 var name = convert(cmd, using: basename)
+var reset = false                   ///< reset and recreate config
+var alwaysPrintQR = false           ///< always display QR code in terminal
 var verbosity = 1                   ///< verbosity level
 var port = 14443                    ///< UDP listen port
 var outp = 14442                    ///< UDP transmit port
 var host = "127.0.0.1"              ///< Controller host
-var pin = "123-44-321"
+var pin = Device.SetupCode.random   ///< start with a random setup code
+var pinSpecified = false            ///< pin was given on the command line
 var vendor = "Orvibo"
 var type = "UDP Bridge"
 var serial = "234"
-var version = "1.0.0"
+var version = "1.1.0"
 var kind = AccessoryKind.outlet
+var customConfig: String?
 
 fileprivate func usage() -> Never {
     print("Usage: \(cmd) <options>")
     print("Options:")
+    print("  -c <config_file>   custom JSON configuration file to use")
     print("  -d                 print debug output")
     print("  -f <version>       firmware version [\(version)]")
     print("  -h <host>          host device [\(host)]")
@@ -41,17 +46,20 @@ fileprivate func usage() -> Never {
     print("  -n <name>          name of the HomeKit bridge [\(name)]")
     print("  -p <port>          broadcast to <port> instead of \(outp)")
     print("  -q                 turn off all non-critical logging output")
-    print("  -s <SECRET_PIN>    HomeKit PIN for authentication [\(pin)]")
+    print("  -Q                 print QR code (default unless -S is passed in)")
+    print("  -R                 reset and recreate configuration")
+    print("  -s <SECRET_PIN>    HomeKit PIN for authentication [random]")
     print("  -S <serial>        Device serial number [\(serial)]")
     print("  -t <type>          name of the model/type [\(type)]")
     print("  -v                 increase logging verbosity\n")
     exit(EXIT_FAILURE)
 }
 
-while let result = get(options: "df:h:k:l:m:n:p:qs:S:t:v") {
+while let result = get(options: "c:df:h:k:l:m:n:p:qQRs:S:t:v") {
     let option = result.0
     let arg = result.1
     switch option {
+    case "c": customConfig = arg
     case "d": verbosity = 9
     case "f": version = arg!
     case "h": host = arg!
@@ -65,7 +73,8 @@ while let result = get(options: "df:h:k:l:m:n:p:qs:S:t:v") {
         outp = p
     } else { usage() }
     case "q": verbosity  = 0
-    case "s": pin = arg!
+    case "Q": alwaysPrintQR = true
+    case "s": pin = .override(arg!) ; pinSpecified = true
     case "S": serial = arg!
     case "t": type = arg!
     case "v": verbosity += 1
@@ -76,25 +85,19 @@ while let result = get(options: "df:h:k:l:m:n:p:qs:S:t:v") {
 }
 
 let fm = FileManager.default
-let dbPath = try! fm.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(name).path
-let dbExists = fm.fileExists(atPath: dbPath)
-let db: FileStorage
-do {
-    db = try FileStorage(path: dbPath)
-} catch {
-    fputs("Cannot open file storage at \(dbPath)\n", stderr)
-    exit(EXIT_FAILURE)
-}
-if !dbExists { RunLoop.main.run(until: Date(timeIntervalSinceNow: 5)) }
+let config = try! customConfig ?? fm.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent(name).appendingPathComponent("configuration.json").path
+let configExists = fm.fileExists(atPath: config)
+let db = FileStorage(filename: config)
+if !configExists { RunLoop.main.run(until: Date(timeIntervalSinceNow: 2)) }
 
-let serviceInfo = Service.Info(name: name, manufacturer: vendor, model: type, serialNumber: serial, firmwareRevision: version)
-let outlet: Accessory
+let serviceInfo = Service.Info(name: name, serialNumber: serial, manufacturer: vendor, model: type, firmwareRevision: version)
+let accessory: Accessory
 switch kind {
-case .light: outlet = Accessory.Lightbulb(info: serviceInfo)
-case .outlet: outlet = Accessory.Outlet(info: serviceInfo)
-case .switch: outlet = Accessory.Switch(info: serviceInfo)
+case .light:  accessory = Accessory.Lightbulb(info: serviceInfo)
+case .outlet: accessory = Accessory.Outlet(info: serviceInfo)
+case .switch: accessory = Accessory.Switch(info: serviceInfo)
 }
-let device = Device(bridgeInfo: serviceInfo, setupCode: pin, storage: db, accessories: [outlet])
+let device = Device(bridgeInfo: Service.Info(name: name + "-bridge", serialNumber: serial, manufacturer: vendor, model: type, firmwareRevision: version), setupCode: pin, storage: db, accessories: [accessory])
 
 var active = true
 signal(SIGINT) { sig in
@@ -109,13 +112,6 @@ signal(SIGINT) { sig in
 let udpStatus  = Channel<Substring>(capacity: 1)
 let udpIn  = try UDPSocket(bind: "0.0.0.0", port: UDPPort(port))
 let udpOut = try UDPSocket(port: UDPPort(outp))
-let server = try Server(device: device, port: 0)
-server.start()
-
-func system(_ command: String, _ args: String...) {
-    let process = Process.launchedProcess(launchPath: command, arguments: args)
-    process.waitUntilExit()
-}
 
 func sendUDP(_ content: String) { udpOut.send(content, to: host) }
 
@@ -130,29 +126,55 @@ func toUDP(status value: Bool?) {
 func fromUDP(status: Bool?) {
     DispatchQueue.main.async {
         if verbosity > 0 { print("Status: \(String(describing: status))") }
-        switch outlet {
-        case let light as Accessory.Lightbulb: light.lightbulb.on.value = status
-        case let outlet as Accessory.Outlet: outlet.outlet.on.value = status
-        case let `switch` as Accessory.Switch: `switch`.switch.on.value = status
+        switch accessory {
+        case let a as Accessory.Lightbulb: a.lightbulb.on.value = status
+        case let a as Accessory.Outlet:    a.outlet.on.value    = status
+        case let a as Accessory.Switch:    a.switch.on.value    = status
         default: return
         }
     }
 }
 
+class HAPDeviceDelegate: DeviceDelegate {
+    func didRequestIdentification() {
+        print(" *** Bridge Identification: \(name)-bridge")
+    }
+    func didRequestIdentificationOf(_ accessory: Accessory) {
+        print(" *** Accessory Identification: \(accessory.info.name.value ?? name)")
+    }
+    func characteristicListenerDidSubscribe(_ accessory: Accessory, service: Service, characteristic: AnyCharacteristic) {
+        print("Subscription for characteristic \(characteristic) for service \(service.type) of accessory \(accessory.info.name.value ?? name)")
+    }
+    func characteristicListenerDidUnsubscribe(_ accessory: Accessory, service: Service, characteristic: AnyCharacteristic) {
+        print("Unsubscribe received for characteristic \(characteristic) of service \(service.type) of accessory \(accessory.info.name.value ?? name)")
+    }
+    func characteristic<T>(_ characteristic: GenericCharacteristic<T>, ofService service: Service, ofAccessory accessory: Accessory, didChangeValue newValue: T?) {
+        print(" --> Characteristic \(characteristic) of service \(service.type) of accessory \(accessory.info.name.value ?? name) changed to: \(String(describing: newValue))")
+        switch newValue {
+        case let s as Bool?: toUDP(status: s)
+        default: print("Unknown value")
+        }
+    }
+}
+let delegate = HAPDeviceDelegate()
+device.delegate = delegate
+
+let server = try Server(device: device, port: 0)
+server.start()
+
+if alwaysPrintQR || !pinSpecified {
+    print("\nQR Code for pairing:\n")
+    print(device.setupQRCode.asText)
+    print()
+}
+
 func status() -> Bool? {
-    switch outlet {
+    switch accessory {
     case let light as Accessory.Lightbulb: return light.lightbulb.on.value
     case let outlet as Accessory.Outlet: return outlet.outlet.on.value
     case let `switch` as Accessory.Switch: return `switch`.switch.on.value
     default: return nil
     }
-}
-
-switch outlet {
-case let light as Accessory.Lightbulb: light.lightbulb.on.onValueChange.append(toUDP)
-case let outlet as Accessory.Outlet: outlet.outlet.on.onValueChange.append(toUDP)
-case let `switch` as Accessory.Switch: `switch`.switch.on.onValueChange.append(toUDP)
-default: print("Cannot subscribe to changes for unknown accessory type '\(kind)'")
 }
 
 var readQueue = DispatchQueue(label: "orvibo.udp.read")
@@ -168,23 +190,24 @@ let udpSource = udpIn.onString(queue: readQueue) { content, endPoint in
 fromUDP(status: nil)
 toUDP(status: nil)
 
-let countDown = 60/2
-var count = countDown
-while active {
-    RunLoop.current.run(until: Date().addingTimeInterval(2))
-    count -= 1
-    guard count <= 0 else { continue }
-    count = countDown
-    guard let currentStatus = status() else {
-        sendUDP("q\n")
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
-            toUDP(status: nil)
+withExtendedLifetime([delegate]) {
+    let countDown = 60/2
+    var count = countDown
+    while active {
+        RunLoop.current.run(until: Date().addingTimeInterval(2))
+        count -= 1
+        guard count <= 0 else { continue }
+        count = countDown
+        guard status() != nil else {
+            sendUDP("q\n")
+            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)) {
+                toUDP(status: nil)
+            }
+            continue
         }
-        continue
     }
 }
 
 if verbosity > 2 { fputs("Stopping server.\n", stderr) }
 server.stop()
 if verbosity > 0 { fputs("Exiting.\n", stderr) }
-
